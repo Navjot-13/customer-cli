9c2350c1711251f2efefc57f3ae74c348bfe8f83
Navjot <cs19b030@iittp.ac.in>
2022-02-12 11:25:28 +0530

    added commands

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
+
 const db = mongoose.connect('mongodb://localhost:27017/customercli');
 
 const Customer = require('./models/customer.schema');
@@ -7,7 +9,7 @@ const Customer = require('./models/customer.schema');
 const addCustomer = (customer) => {
     Customer.create(customer).then(customer => {
         console.info('new customer added');
-        db.close();
+        // db.close();
     });
 }
 
@@ -16,9 +18,40 @@ const findCustomer = (name) => {
     Customer.find({$or : [{firstName : search}, {lastName : search}]})
     .then(customer => {
         console.info(customer);
-        console.info(`${customer.length} matches`);
-        db.close();
+        console.info(`${customer.length} matches found`);
+        // db.close();
     });
 }
 
-module.exports = {addCustomer, findCustomer};
+const updateCustomer = (_id, customer) => {
+    Customer.updateOne({ _id }, customer)
+        .then(customer => {
+            console.info('customer updated');
+            // db.close()
+        })
+}
+
+const removeCustomer = (_id) => {
+    Customer.remove({ _id })
+        .then(customer => {
+            console.info('customer removed');
+            // db.close()
+        })
+}
+
+const listCustomers = () => {
+    Customer.find()
+        .then(customers => {
+            console.info(customers);
+            console.info(`${customers.length} customers`);
+            // db.close()
+        })
+}
+
+module.exports = {
+    addCustomer, 
+    findCustomer, 
+    updateCustomer, 
+    removeCustomer, 
+    listCustomers
+};
